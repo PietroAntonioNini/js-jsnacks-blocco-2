763124882829f3eff53cf511394aa5e83bd72ea4
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-05 17:42:10 +0100

    Programma funzionante

---
+++ snack2/js/script.js
@@ -0,0 +1,26 @@
+//PROGRAMMA INDOVINA NUMERO TRA 1 E 100
+
+// Genera un numero casuale tra 1 e 100
+let numberRandom = Math.floor(Math.random() * 100) + 1;
+console.log(numberRandom);
+
+// Chiedo all'utente di indovinare il numero
+let numberUser = Number(prompt("Indovina un numero tra 1 e 100"));
+console.log(numberUser);
+
+// Continua a chiedere finché l'utente non indovina il numero
+while (numberUser !== numberRandom) {
+    if (numberUser > numberRandom) {
+        alert("Il numero è troppo alto!");
+    } else if (numberUser < numberRandom) {
+        alert("Il numero è troppo basso!");
+    }
+
+    // Chiedi all'utente di rreinserire il numero
+    numberUser = Number(prompt("Prova un altro numero"));
+}
+
+// Quando l'utente indovina il numero, faglielo sapere
+document.querySelector("#user").innerText += `Numero utente= ${numberUser}`;
+document.querySelector("#random").innerText += `Numero random= ${numberRandom}`;
+document.querySelector("#result").innerText = "Complimenti! Hai indovinato il numero!"
